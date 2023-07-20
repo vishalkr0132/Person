c28f70a9ed1fb77b69a4833e1877a140b08b523a
Vishal Kumar <131611139+vishalkr0132@users.noreply.github.com>
2023-07-21 03:39:38 +0530

    Update settings.py

--- Personl/settings.py
+++ Personl/settings.py
@@ -130,5 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ]
 
 import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
